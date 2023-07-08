73e3c61dcac921391c93986f5d2b741c219ea26c
Mehmet Müjde <freelancer@mehmetmuejde.com>
2023-07-08 21:24:17 +0200

    Adds random cell generation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { Box, Button, Group } from '@mantine/core';
+import { Box, Button, Center, Chip, Group, Header, Title } from '@mantine/core';
 import { produce } from 'immer';
 import { useCallback, useRef, useState } from 'react';
 import { Toaster, toast } from 'react-hot-toast';
 
-const COL_SIZE = 50;
-const ROW_SIZE = 50;
+const COL_SIZE = 40;
+const ROW_SIZE = 25;
 
 const operations = [
   [0, 1],
@@ -19,6 +19,7 @@ const operations = [
 
 const App = () => {
   const [isRunning, setIsRunning] = useState(false);
+  const [generation, setGeneration] = useState(0);
 
   const getBlankGrid = () => {
     let rows = [];
@@ -32,6 +33,9 @@ const App = () => {
 
   const runningRef = useRef(isRunning);
   runningRef.current = isRunning;
+
+  const generationRef = useRef(generation);
+  generationRef.current = generation;
 
   const runSimulation = useCallback(() => {
     if (!runningRef.current) {
@@ -68,26 +72,62 @@ const App = () => {
       });
     });
 
+    setGeneration(generationRef.current + 1);
+
     setTimeout(runSimulation, 500);
   }, []);
 
   const startHandler = () => {
+    if (checkGameField()) {
+      toast.error('Game field is empty! Please, fill it with cells.', {
+        id: 'empty',
+      });
+      return;
+    }
+
     setIsRunning(!isRunning);
 
     if (!isRunning) {
-      toast.success('Simulation is started!');
+      toast.success('Simulation is started!', { id: 'start' });
       runningRef.current = true;
       runSimulation();
+    } else {
+      toast.success('Simulation is stopped!', { id: 'stop' });
     }
   };
 
-  const pauseHandler = () => {};
+  const checkGameField = () => {
+    let isGameFieldEmpty = true;
+    for (let i = 0; i < ROW_SIZE; i++) {
+      for (let k = 0; k < COL_SIZE; k++) {
+        if (grid[i][k] === 1) {
+          isGameFieldEmpty = false;
+        }
+      }
+    }
+    return isGameFieldEmpty;
+  };
 
-  const randomHandler = () => {};
+  const randomHandler = () => {
+    const rows = [];
+    for (let i = 0; i < ROW_SIZE; i++) {
+      rows.push(
+        Array.from(Array(COL_SIZE), () => (Math.random() > 0.7 ? 1 : 0))
+      );
+    }
+    setGrid(rows);
+    toast.success('Game field initialized with random cells!', {
+      id: 'random',
+    });
+  };
 
   const clearHandler = () => {
+    setIsRunning(false);
+    setGeneration(0);
     setGrid(getBlankGrid);
-    toast.error('Game field is cleared!');
+    toast.error('Game field is cleared!', {
+      id: 'clear',
+    });
   };
 
   const cellClickHandler = (i: number, k: number) => {
@@ -99,52 +139,86 @@ const App = () => {
 
   return (
     <Box px={50}>
-      <Group
-        position="right"
+      <Header
+        height={50}
         sx={{
-          height: 50,
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          position: 'fixed',
+          top: 0,
         }}
       >
-        <Button onClick={startHandler}>
-          {isRunning ? 'Stop the simulation' : 'Start the simulation'}
-        </Button>
-        <Button onClick={pauseHandler} color="yellow">
-          Pause
-        </Button>
-        <Button onClick={randomHandler} color="orange" variant="outline">
-          Random cells
-        </Button>
-        <Button onClick={clearHandler} color="red" variant="outline">
-          Clear game field
-        </Button>
-      </Group>
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: `repeat(${COL_SIZE}, 30px)`,
-        }}
-      >
-        {grid.map((rows, i) =>
-          rows.map((col, k) => (
-            <Box
-              key={`${i}-${k}`}
-              sx={{
-                width: '30px',
-                height: '30px',
-                backgroundColor: grid[i][k] ? 'black' : undefined,
-                border: 'solid 1px black',
-                ':hover': {
-                  backgroundColor: grid[i][k] ? 'black' : 'gray',
-                  cursor: 'pointer',
-                },
-              }}
-              onClick={() => {
-                cellClickHandler(i, k);
-              }}
-            ></Box>
-          ))
-        )}
-      </div>
+        <Title order={3} pl={20}>
+          Game of Life
+        </Title>
+        <Group pr={20}>
+          <Button onClick={startHandler} size="xs">
+            {isRunning ? 'Stop the simulation' : 'Start the simulation'}
+          </Button>
+          <Button
+            onClick={randomHandler}
+            color="orange"
+            variant="outline"
+            size="xs"
+            disabled={isRunning}
+          >
+            Initiate with random cells
+          </Button>
+          <Button
+            onClick={clearHandler}
+            color="red"
+            variant="outline"
+            size="xs"
+            disabled={isRunning}
+          >
+            Clear all cells
+          </Button>
+        </Group>
+      </Header>
+      <Center pt={80}>
+        <Group>
+          <Chip checked={false} color="red" variant="filled">
+            Generation: {generation}
+          </Chip>
+          <Chip
+            checked={false}
+            color={isRunning ? 'green' : 'red'}
+            variant="filled"
+          >
+            {isRunning ? 'Simulation is running' : "Simulation isn't running"}
+          </Chip>
+        </Group>
+      </Center>
+      <Center pt={20}>
+        <Box
+          sx={{
+            display: 'grid',
+            gridTemplateColumns: `repeat(${COL_SIZE}, 30px)`,
+          }}
+        >
+          {grid.map((rows, i) =>
+            rows.map((col, k) => (
+              <Box
+                key={`${i}-${k}`}
+                sx={{
+                  width: '30px',
+                  height: '30px',
+                  backgroundColor: grid[i][k] ? 'black' : undefined,
+                  border: 'solid 1px black',
+                  ':hover': {
+                    backgroundColor: grid[i][k] ? 'black' : 'gray',
+                    cursor: 'pointer',
+                  },
+                }}
+                onClick={() => {
+                  cellClickHandler(i, k);
+                }}
+              ></Box>
+            ))
+          )}
+        </Box>
+      </Center>
       <Toaster position="bottom-center" />
     </Box>
   );
